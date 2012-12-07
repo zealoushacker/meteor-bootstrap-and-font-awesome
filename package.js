d53aa24c98ad90a31d36a95409d455b462bb87f4
Alex Notov <anotov@gmail.com>
2012-12-06 22:37:53 -0800

    Add font-awesome font files

--- package.js
+++ package.js
@@ -5,6 +5,10 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
+  api.add_files(path.join('font', 'fontawesome-webfont.eot'), 'client');
+  api.add_files(path.join('font', 'fontawesome-webfont.woff'), 'client');
+  api.add_files(path.join('font', 'fontawesome-webfont.ttf'), 'client');
+  api.add_files(path.join('font', 'fontawesome-webfont.svg'), 'client');
   api.add_files(path.join('bootstrap', 'less', 'bootstrap.less'), 'client');
   api.add_files(path.join('less', 'responsive.less'), 'client');
   api.add_files(path.join('js', '*'), 'client');
