eebb7d12bdd5d3d9536aabc6dc84c768a0dcdac9
Alex Notov <anotov@gmail.com>
2012-12-06 23:14:46 -0800

    Add bootstrap and fontawesome
    Update package js

--- package.js
+++ package.js
@@ -5,16 +5,12 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
+  api.add_files(path.join('css', 'bootstrap.min.css'), 'client');
+  api.add_files(path.join('js', 'bootstrap.min.js'), 'client');
+
+  api.add_files(path.join('font', 'fontawesome-webfont.eot'), 'client');
   api.add_files(path.join('font', 'fontawesome-webfont.eot'), 'client');
   api.add_files(path.join('font', 'fontawesome-webfont.woff'), 'client');
   api.add_files(path.join('font', 'fontawesome-webfont.ttf'), 'client');
-  api.add_files(path.join('font', 'fontawesome-webfont.svg'), 'client');
-  api.add_files(path.join('bootstrap', 'less', 'bootstrap.less'), 'client');
-  api.add_files(path.join('less', 'responsive.less'), 'client');
-  api.add_files(path.join('js', '*'), 'client');
-
-  // XXX this makes the paths to the icon sets absolute. it needs
-  // to be included _after_ the standard bootstrap css so
-  // that its styles take precedence.
-  // api.add_files(path.join('css', 'bootstrap-override.css'), 'client');
+  api.add_files(path.join('css', 'font-awesome.css'), 'client');
 });
