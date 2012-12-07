1c4cef00ee0d22d49bcffb0c2bea2fd38b7a8f34
Alex Notov <anotov@gmail.com>
2012-12-07 01:03:56 -0800

    Revert "Attempt to use absolute font paths"
    This reverts commit ea25b2ff8b3e241c631874587c8ffc0de992236a.

--- package.js
+++ package.js
@@ -8,9 +8,9 @@ Package.on_use(function (api) {
   api.add_files(path.join('css', 'bootstrap.min.css'), 'client');
   api.add_files(path.join('js', 'bootstrap.min.js'), 'client');
 
-  api.add_files('/packages/bootstrap-with-font-awesome/font/fontawesome-webfont.eot', 'client');
-  api.add_files('/packages/bootstrap-with-font-awesome/font/fontawesome-webfont.eot', 'client');
-  api.add_files('/packages/bootstrap-with-font-awesome/font/fontawesome-webfont.woff', 'client');
-  api.add_files('/packages/bootstrap-with-font-awesome/font/fontawesome-webfont.ttf', 'client');
+  api.add_files(path.join('font', 'fontawesome-webfont.eot'), 'client');
+  api.add_files(path.join('font', 'fontawesome-webfont.eot'), 'client');
+  api.add_files(path.join('font', 'fontawesome-webfont.woff'), 'client');
+  api.add_files(path.join('font', 'fontawesome-webfont.ttf'), 'client');
   api.add_files(path.join('css', 'font-awesome.css'), 'client');
 });
