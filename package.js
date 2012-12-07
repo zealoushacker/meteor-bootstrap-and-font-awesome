1c6d6b00e4e5cd4a6ddc78d496297be08ad8d9d9
Alex Notov <anotov@gmail.com>
2012-12-07 01:03:06 -0800

    Revert "Heroku problems; Attempting to use non-minified versions."
    This reverts commit 6855e97f9a1b28a941ebee530280db672d920dcd.

--- package.js
+++ package.js
@@ -5,8 +5,8 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.add_files(path.join('css', 'bootstrap.css'), 'client');
-  api.add_files(path.join('js', 'bootstrap.js'), 'client');
+  api.add_files(path.join('css', 'bootstrap.min.css'), 'client');
+  api.add_files(path.join('js', 'bootstrap.min.js'), 'client');
 
   api.add_files('/packages/bootstrap-with-font-awesome/font/fontawesome-webfont.eot', 'client');
   api.add_files('/packages/bootstrap-with-font-awesome/font/fontawesome-webfont.eot', 'client');
