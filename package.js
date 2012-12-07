77e1e53198e9d564cd976d7c6628fbf3ee7a9a66
Alex Notov <anotov@gmail.com>
2012-12-06 22:31:32 -0800

    Meteorite/meteor package api files

---
+++ package.js
@@ -0,0 +1,16 @@
+var path = require('path');
+
+Package.describe({
+  summary: "Bootstrap with font-awesome included!"
+});
+
+Package.on_use(function (api) {
+  api.add_files(path.join('bootstrap', 'less', 'bootstrap.less'), 'client');
+  api.add_files(path.join('less', 'responsive.less'), 'client');
+  api.add_files(path.join('js', '*'), 'client');
+
+  // XXX this makes the paths to the icon sets absolute. it needs
+  // to be included _after_ the standard bootstrap css so
+  // that its styles take precedence.
+  // api.add_files(path.join('css', 'bootstrap-override.css'), 'client');
+});
